9a172809b9a533bf42bf5706b9f7d8c1f5e04117
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2020-12-10 11:42:17 +0100

    Mateusz Navigator, Ania Driver

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const config = {
   isDrawing: false,
-  hue: `hsl(0,100%,50%)`,
+  hue: 0,
   lineJoin: 'round',
   lineCap: 'round',
-  lineWidth: 10,
+  lineWidth: 1,
   canvasWidth: window.innerWidth,
   canvasHeight: window.innerHeight,
+  direction: true,
 };
 
 document.addEventListener('DOMContentLoaded', () => init());
@@ -16,10 +17,13 @@ function init() {
   canvas.height = config.canvasHeight;
   const ctx = canvas.getContext('2d');
 
+  const btn = document.querySelector('.btn');
+
   canvas.addEventListener('mousemove', (e) => draw(e, ctx));
-  //   canvas.addEventListener('mouseout', stopDrawing);
-  canvas.addEventListener('mousedown', (e) => startPosition(e, ctx));
+  canvas.addEventListener('mouseout', stopDrawing);
+  canvas.addEventListener('mousedown', (e) => startPosition(e));
   canvas.addEventListener('mouseup', stopDrawing);
+  btn.addEventListener('click', (ctx, canvas) => clearCanvas(ctx, canvas));
 }
 
 let lastX = 0;
@@ -29,26 +33,44 @@ function draw(e, ctx) {
   if (!config.isDrawing) {
     return;
   } else {
-    ctx.strokeStyle = 'green';
+    ctx.strokeStyle = `hsl(${config.hue}, 100%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     lastX = e.offsetX;
-    lastY = e.offsetY; // tu problem
+    lastY = e.offsetY;
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     ctx.lineJoin = config.lineJoin;
     ctx.lineCap = config.lineCap;
     ctx.lineWidth = config.lineWidth;
+    ++config.hue;
+    if (config.hue === 360) {
+      config.hue = 0;
+    }
+    if (config.lineWidth === 50) {
+      config.direction = false;
+    } else if (config.lineWidth === 1) {
+      config.direction = true;
+    }
+
+    if (config.direction) {
+      ++config.lineWidth;
+    } else {
+      --config.lineWidth;
+    }
   }
 }
 
-function startPosition(e, ctx) {
+function startPosition(e) {
   config.isDrawing = true;
-  // ctx.beginPath();
   lastX = e.offsetX;
   lastY = e.offsetY;
 }
 
 function stopDrawing() {
   config.isDrawing = false;
+}
+
+function clearCanvas(ctx, canvas) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
